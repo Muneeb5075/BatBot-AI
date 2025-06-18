5e9d5e491d30c2f36bca241d647b5798a37fe3f3
M.Muneeb <monirao2944@gmail.com>
2025-06-19 01:49:28 +0500

    add project
    add project

--- README.md
+++ README.md
@@ -1,16 +1,12 @@
-#  BatBot – Your Smart AI Companion
-BatBot is an intelligent AI assistant designed to help you with everything from answering questions to generating creative content. Whether you're looking for quick information, writing support, coding help, or just a friendly conversation, BatBot is always ready to chat.
+# React + Vite
 
-💡 Key Features:
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
-Natural conversations in English and Urdu
+Currently, two official plugins are available:
 
-Instant answers to all your questions
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
-Helps with writing, summaries, and translations
+## Expanding the ESLint configuration
 
-Coding support and logical problem-solving
-
-Always available — 24/7 smart assistance
-
-Powered by advanced AI technology, BatBot understands your needs and delivers clear, helpful, and human-like responses. Start chatting today and explore the future of intelligent conversations!
+If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
---
+++ src/context/Context.jsx
@@ -0,0 +1,95 @@
+import { createContext, use } from "react";
+import runChat from "../config/gemini";
+import { useState } from "react";
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+    const[input, setInput] = useState("");
+    const[recentPrompt, setRecentPrompt] = useState("");
+    const[prevPrompts, setPrevPrompts] = useState([]);
+    const[showResult, setShowResult] = useState(false);
+    const[loading, setLoading] = useState(false);
+    const[resultData, setResultData] = useState("");
+
+    const delayPara = (index, nextWord) => {
+        setTimeout(function()  {
+            setResultData(prev=>prev+nextWord)
+        },75*index)
+
+    }
+
+    const onSent = async (prompt) => {
+
+        setResultData("")
+        setLoading(true)
+        setShowResult(true)
+        setRecentPrompt(input)
+        const response = await runChat(input)
+        let responseArray = response.split("**");
+        let newResponse = "";
+        for (let i =0 ; i < responseArray.length; i++)
+        {
+            if (i === 0 || i%2 !== 1) {
+                newResponse += responseArray[i];
+            } 
+            else {
+                newResponse += "<b>"+responseArray[i]+"</b>";
+            }
+        }
+        // let newResponse2 = newResponse.split("*").join("</br>")
+        // let newResponseArray = newResponse2.split("");
+        // for (let i=0; i<newResponseArray.length;i++) 
+        // {
+        //     const nextWord = newResponseArray[i];
+        //     delayPara(i,nextWord+" ");
+        // }
+        setResultData(newResponse);
+        setLoading(false)
+        setInput("")
+
+}
+
+    const contextValue ={
+        prevPrompts,
+        setPrevPrompts,
+        onSent,
+        setRecentPrompt,
+        recentPrompt,
+        showResult,
+        loading,
+        resultData,
+        input,
+        setInput,
+    }
+
+    return(
+        <Context.Provider value={contextValue}>
+            {props.children}
+        </Context.Provider>
+    )
+}
+
+export default ContextProvider
+
+
+
+// let responseArray = response.split("**");
+        // let newResponse;
+        // for (let i =0 ; i < responseArray.length; i++)
+        // {
+        //     if (i === 0 || i%2 !== 1) {
+        //         newResponse += responseArray[i];
+        //     } 
+        //     else {
+        //         newResponse += "<b>"+responseArray[i]+"</b>";
+        //     }
+        // }
+
+// let newResponse2 = newResponse.split("*").join("</b>")
+//         let newResonseArray = newResponse2.split("");
+//         for (let i = 0; i < newResonseArray.length; i++) 
+//         {
+//             const nextWord = newResonseArray[i];
+//             delayPara(i, nextWord+"");
+//         }

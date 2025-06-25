13f25ff7d1a87ca7e369fcbccb414c7b3b46305f
M.Muneeb <monirao2944@gmail.com>
2025-06-25 20:17:09 +0500

    config
    config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/BatBot-AI/', // <- your repo name
+  base: '/', // <- your repo name
   plugins: [react()],
 })
